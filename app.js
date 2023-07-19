47207a10faa6e045ad4136a86e18e32caf5ea39d
Shofiq Saif <sislam161079@gmail.com>
2023-07-19 22:05:31 +0600

    Mongoose model create

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("article", articleSchema);
 
 app.route("/articles")
 .get((req,res)=>{
